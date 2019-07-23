e291755b34f81c031970a7c956593f77fd60f1ed
Arsteinberg <arsteinberg93@gmail.com>
2019-07-23 19:10:51 -0400

    still not appending

--- public/app.js
+++ public/app.js
@@ -1,10 +1,9 @@
 function displayResults(scrapedData) {
-    // First, empty the table
+
     $("tbody").empty();
 
-    // Then, for each entry of that json...
     scrapedData.forEach(function(story) {
-        // Append each of the animal's properties to the table
+
         var tr = $("<tr>").append(
             $("<td>").text(story.title),
             $("<td>").text(story.link),
@@ -15,6 +14,6 @@ function displayResults(scrapedData) {
 }
 
 $.getJSON("/", function(data) {
-    // Call our function to generate a table body
+
     displayResults(data);
 });
